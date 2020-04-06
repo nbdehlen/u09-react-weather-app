4020407fa9bf34da75d2708f2e0e26054846f228
Lacki Kritsimas <l.kritsimas@gmail.com>
2020-04-06 16:11:12 +0200

    Install react-icons and more
    * Install react-icons
    * Change padding in Card component
    * Style CurrentWeather component

--- src/components/CurrentWeather.tsx
+++ src/components/CurrentWeather.tsx
@@ -1,57 +1,65 @@
 import React from 'react';
+import { FaWind } from 'react-icons/fa';
+import { WiHumidity, WiBarometer } from 'react-icons/wi';
 import { Card } from './Card';
+import { round, degreeDescription } from '../utilities';
 import { Weather } from '../interfaces';
-import { round } from '../utilities';
 
-export const CurrentWeather: React.FC<any> = ({ data, unit }: any) => {
+interface Props {
+  data: Weather;
+  unit: string;
+}
+
+export const CurrentWeather: React.FC<Props> = ({ data, unit }: Props) => {
   const weather = data.weather ? data.weather[0] : null;
 
   return (
     <>
       {weather ? (
-        <Card>
+        <Card title="Current weather" titleSmall>
           <div className="flex flex-col justify-center items-center text-center">
-            <div className="flex items-center justify-evenly w-full">
+            <div className="flex flex-wrap items-center justify-evenly w-full">
               <img
                 src={`http://openweathermap.org/img/wn/${weather.icon}@2x.png`}
                 alt=""
               />
               <div>
                 <p className="text-white text-4xl">
-                  <span className="mr-1">{round(data.main.temp)}</span>
+                  <span className="mr-1">{round(data.main?.temp)}</span>
                   <sup className="text-2xl">
                     &deg;
                     {unit}
                   </sup>
                 </p>
                 <p>
-                  Feels like:
+                  Feels like
                   {' '}
-                  {data.main.feels_like}
-                  &deg;
-                  {unit}
+                  <span className="text-gray-400">
+                    {round(data.main?.feels_like)}
+                    &deg;
+                    {unit}
+                  </span>
                 </p>
               </div>
             </div>
-            <p>
-              Low:
-              {round(data.main.temp_min)}
-              &deg;
-              {unit}
-            </p>
-            <p>
-              High:
-              {round(data.main.temp_max)}
-              &deg;
-              {unit}
-            </p>
-            <div className="flex justify-between w-full">
+            <div className="flex flex-wrap justify-between w-full">
               <div className="flex items-center">
-                {`${data.main.pressure} hPa`}
-                <sup>2</sup>
+                <WiHumidity className="text-3xl" />
+                <span className="text-white">{`${data.main?.humidity}%`}</span>
               </div>
               <div className="flex items-center">
-                {`${data.wind.speed} ${unit === 'C' ? 'km/h' : 'mp/h'}`}
+                <WiBarometer className="text-3xl" />
+                <span className="text-white">
+                  {`${data.main?.pressure} hPa`}
+                </span>
+              </div>
+              <div className="flex items-center">
+                <FaWind className="text-xl mr-2" />
+                <span className="text-white">
+                  {`${round(data.wind?.speed)} ${degreeDescription(
+                    data.wind?.deg,
+                  )} ${unit === 'C' ? 'm/s' : 'm/h'}`}
+                </span>
               </div>
             </div>
           </div>
--- src/utilities/index.tsx
+++ src/utilities/index.tsx
@@ -1,3 +1,8 @@
-export const round = (value: number) => (
-  value ? Math.round(value * 10) / 10 : null
+export const round = (value: number | undefined): number | undefined => (
+  value ? Math.round(value * 10) / 10 : undefined
 );
+
+export const degreeDescription = (degrees: number | undefined): string | undefined => {
+  const compassSector = ['N', 'NNE', 'NE', 'ENE', 'E', 'ESE', 'SE', 'S', 'SSW', 'SW', 'WSW', 'W', 'WNW', 'NW', 'NNW', 'N'];
+  return degrees ? compassSector[parseFloat((degrees / 22.5).toFixed(0)) - 1] : undefined;
+};
